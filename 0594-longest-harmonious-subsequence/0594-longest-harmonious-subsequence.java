33cff812d051cfa8e53609d615031356fcd60271
Anshika Mishra <anshikamishra47021@gmail.com>
2026-07-03 13:18:34 +0530

    Time: 128 ms (5.07%), Space: 48.3 MB (25.63%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -3,16 +3,32 @@ class Solution {
        Arrays.sort(nums);
        int len=0;
       
-       for(int i=0;i<nums.length;i++){
-        for(int j=i+1;j<nums.length;j++){
-            if(nums[j]-nums[i]>1){
-                break;
-            }
-            else if(nums[j]-nums[i]==1){
-                  len=Math.max(len,j-i+1);
-            }
-        }
-       } 
+    //    for(int i=0;i<nums.length;i++){
+    //     for(int j=i+1;j<nums.length;j++){
+    //         if(nums[j]-nums[i]>1){
+    //             break;
+    //         }
+    //         else if(nums[j]-nums[i]==1){
+    //               len=Math.max(len,j-i+1);
+    //         }
+    //     }
+    //    } 
+    int i=0;
+    int j=i+1;
+    while(j<nums.length){
+     if(nums[j]-nums[i]>1){
+        i++;
+     }
+     else if(nums[j]-nums[i]==0){
+        j++;
+     }
+     else if(nums[j]-nums[i]==1)
+     {
+      len=Math.max(len,j-i+1);
+      j++;
+     }
+   
+    }
        return len;
     }
 }
