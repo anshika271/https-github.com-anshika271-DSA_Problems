bf4b6c79354866b9c295d7939ffc211e1358f2d0
Anshika Mishra <anshikamishra47021@gmail.com>
2026-07-12 03:43:56 +0530

    Time: 3 ms (98.4%), Space: 46.6 MB (31.9%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.java
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -1,14 +1,30 @@
 class Solution {
+    public static void pk(String s,int i,StringBuilder sb){
+        if(i==s.length()){
+            return;
+        }
+         
+         if(s.charAt(i)=='i'){
+           sb.reverse(); 
+         }
+         else{
+           sb.append(s.charAt(i)); 
+         }
+         pk(s,i+1,sb);
+    }
     public String finalString(String s) {
-      StringBuilder sb=new StringBuilder();
-      for(int i=0;i<s.length();i++){
-        if(s.charAt(i)=='i'){
-            sb.reverse();
-        }
-        else{
-            sb.append(s.charAt(i));
-        }
-      }
-      return new String(sb);
+         StringBuilder sb=new StringBuilder();  
+          pk(s,0,sb);
+          return new String(sb);
+    //   StringBuilder sb=new StringBuilder();
+    //   for(int i=0;i<s.length();i++){
+    //     if(s.charAt(i)=='i'){
+    //         sb.reverse();
+    //     }
+    //     else{
+    //         sb.append(s.charAt(i));
+    //     }
+    //   }
+    //   return new String(sb);
     }
 }
