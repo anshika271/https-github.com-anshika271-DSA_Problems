cfb499a2afe6b0df997f3316a620f1d763e66a84
Anshika Mishra <anshikamishra47021@gmail.com>
2026-07-21 02:43:28 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,17 @@
 class Solution {
+    public static int memo(int n, int[]dp){
+if(n<=1){
+    return n;
+}
+if(dp[n]!=-1){
+    return dp[n];
+}
+dp[n]=memo(n-1,dp)+memo(n-2,dp);
+return dp[n];
+    }
     public int fib(int n) {
-        
-      if(n==0){
-        return 0;
-      }  
-      if(n==1){
-        return 1;
-      }
-      int fib=fib(n-1)+fib(n-2);
-return fib;
+     int[]dp=new int[n+1];
+     Arrays.fill(dp,-1);
+     return memo(n,dp);   
     }
 }
