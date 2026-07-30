4991eb88a12e33917190fd17d152708a70b4c7f2
Anshika Mishra <anshikamishra47021@gmail.com>
2026-07-31 02:35:35 +0530

    Time: 1 ms (87.11%), Space: 45.3 MB (90.47%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,6 +1,6 @@
 class Solution {
     List<List<Integer>>ans=new ArrayList<>();
-    public void fun(int[]nums,int idx,ArrayList<Integer>list){
+    public void fun(int[]nums,ArrayList<Integer>list){
        if(nums.length==list.size()){
         ans.add(new ArrayList<>(list));
         return;
@@ -8,14 +8,14 @@ class Solution {
        for(int i=0;i<nums.length;i++){
         if(!list.contains(nums[i])){
             list.add(nums[i]);
-            fun(nums,idx+1,list);
+            fun(nums,list);
             list.remove(list.size()-1);
         }
        }
     }
     public List<List<Integer>> permute(int[] nums) {
        ArrayList<Integer>list=new ArrayList<>();
-fun(nums,0,list);
+fun(nums,list);
 return ans;
     }
 }
