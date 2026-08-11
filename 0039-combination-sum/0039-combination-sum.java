26c83fdbd1592a1b81af7808de31f7eaee9913fe
Anshika Mishra <anshikamishra47021@gmail.com>
2026-08-11 20:19:06 +0530

    Time: 2 ms (96.23%), Space: 45.7 MB (51.45%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,31 +1,27 @@
 class Solution {
     List<List<Integer>>ans=new ArrayList<>();
-    public void fun(int[]candidates,int idx,int target,int sum,ArrayList<Integer>list){
-       
-           if(idx==candidates.length){
-            if(sum==target){
-                ans.add(new ArrayList<>(list));
-            }
-            return ;
-           }
-            if(sum>target){
-                return;
-            }
+    public  void fun(int[]arr,int target,int idx, ArrayList<Integer>list,int sum){
+      if(sum==target) {
+        ans.add(new ArrayList<>(list));
+        return;
+      }
+      if(idx==arr.length){
+        return;
+      }
+    if(sum>target){
+        return;
+    }
+      list.add(arr[idx]);
+      sum=sum+arr[idx];
+      fun(arr,target,idx,list,sum);
 
-        
-        // if(candidates[idx]<=target){
-        list.add(candidates[idx]);
-       sum= sum+candidates[idx];
-        fun(candidates,idx,target,sum,list);
-         list.remove(list.size()-1);
-        
-        sum=sum-candidates[idx];
-          fun(candidates,idx+1,target, sum,list);
-}
-    
+      list.remove(list.size()-1);
+      sum=sum-arr[idx];
+       fun(arr,target,idx+1,list,sum);
+    }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
        ArrayList<Integer>list=new ArrayList<>();
-       fun(candidates,0,target,0,list); 
-       return ans;
+        fun(candidates,target,0,list,0);
+        return ans;
     }
 }
