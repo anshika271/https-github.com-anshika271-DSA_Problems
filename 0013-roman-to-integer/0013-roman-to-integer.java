7d1247e3cc862c869b50eed14a76bdc3e5fd792e
Anshika Mishra <anshikamishra47021@gmail.com>
2026-08-11 21:12:47 +0530

    Time: 5 ms (33.07%), Space: 46.9 MB (14.42%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+      HashMap<Character,Integer>mp=new HashMap<>();
+     mp.put('I',1);
+     mp.put('V',5);
+     mp.put('X',10);
+      mp.put('L',50);
+       mp.put('C',100);
+        mp.put('D',500);
+         mp.put('M',1000);
+         int sum=mp.get(s.charAt(s.length()-1));
+         for(int i=s.length()-2;i>=0;i--){
+            if(mp.get(s.charAt(i))<mp.get(s.charAt(i+1)))
+            {
+           sum=Math.abs(sum-(mp.get(s.charAt(i))));
+            }
+            else{
+                sum=sum+mp.get(s.charAt(i));
+            }
+         }
+         return sum;
+    }
+}
