be0ced5c9d085e701ed210427258067d1b882e7d
Anshika Mishra <anshikamishra47021@gmail.com>
2026-08-18 01:34:17 +0530

    Time: 5 ms (81.45%), Space: 45.3 MB (99.06%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false; 
+        }
+        
+        int n = x;
+        int sum = 0;
+        
+        while (x > 0) {
+            int rem = x % 10;
+            sum = sum * 10 + rem;
+            x = x / 10;
+        }
+        
+        if (sum == n) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
