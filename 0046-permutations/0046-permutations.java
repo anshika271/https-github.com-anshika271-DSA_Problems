a3d0073dabfad6294c1e8f75ae27d080389ea48c
Anshika Mishra <anshikamishra47021@gmail.com>
2026-08-27 20:48:11 +0530

    Time: 2 ms (34.65%), Space: 45.6 MB (28.97%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,21 +1,28 @@
 class Solution {
+   HashSet<Integer>seen=new HashSet<>();
     List<List<Integer>>ans=new ArrayList<>();
-    public void fun(int[]nums,ArrayList<Integer>list){
-       if(nums.length==list.size()){
-        ans.add(new ArrayList<>(list));
-        return;
-       }
-       for(int i=0;i<nums.length;i++){
-        if(!list.contains(nums[i])){
-            list.add(nums[i]);
-            fun(nums,list);
-            list.remove(list.size()-1);
+    public  void fun(int[]nums,ArrayList<Integer>list){
+        if(list.size()==nums.length){
+            ans.add(new ArrayList<>(list));
+            return;
         }
-       }
+
+        for(int i=0;i<nums.length;i++){
+            if(!seen.contains(nums[i])){
+                list.add(nums[i]);
+                seen.add(nums[i]);
+                //explore
+                fun(nums,list);
+                //backtrack
+                list.remove(list.size()-1);
+                seen.remove(nums[i]);
+
+            }
+        }
     }
     public List<List<Integer>> permute(int[] nums) {
-       ArrayList<Integer>list=new ArrayList<>();
-fun(nums,list);
-return ans;
+        ArrayList<Integer>list=new ArrayList<>();
+        fun(nums,list);
+        return ans;
     }
 }
